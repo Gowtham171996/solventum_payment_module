6eed016a0b166ec5036449e7aac5fc554b4e18b6
findxpro <bcgowtham17@gmail.com>
2026-01-04 20:46:00 +0000

    added dynamic doc type

---
+++ solventum_utils/solventum_utils/doctype/qr_code/qr_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Gowtham by Solventum OPC private limited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("QR code", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ solventum_utils/solventum_utils/doctype/qr_code/test_qr_code.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Gowtham by Solventum OPC private limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQRcode(FrappeTestCase):
+	pass
